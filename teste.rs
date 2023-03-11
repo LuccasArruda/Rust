83c06e0a11cf12a9ec57f3ed472a6ce51bb4146f
LuccasArruda <luccasarruda@hotmail.com.br>
2023-03-11 00:32:36 -0300

    Primeiro Commit

---
+++ teste.rs
@@ -0,0 +1,11 @@
+//main é obrigatória para arquivos em binário
+//função main é entry point
+//println() é como se fosse o Escreval
+//println!() tem a ! pois é uma macro
+//este arquivo NÃO ESTÁ COMPILADO!
+//Para compilar, deve-se passar o comando rustc + [caminho do arquivo]
+//Neste caso rustc teste.rs
+// .\[nome do arquivo sem extensão] para rodar o arquivo
+fn main () {
+    println!("Bom dia");
+}
